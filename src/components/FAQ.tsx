ce3b4a27aa451467c8e57b207549ac586a66312c
Joseph Nash Belandres <jnashbelandres@outlook.com>
2024-11-16 00:09:55 +0800

    feat: FAQ

---
+++ src/components/FAQ.tsx
@@ -0,0 +1,60 @@
+import parser from "html-react-parser";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const faqItems = [
+  {
+    question: "Why Unity in Action?",
+    answer:
+      '"Unity in Action" encourages the idea that true progress comes when collaboration is translated into active engagement -- through volunteerism, partnerships, and resource-sharing.',
+  },
+  {
+    question: "How are our beneficiaries being selected?",
+    answer:
+      "Our beneficiaries are typically selected based on criteria that align with the our goals and objectives. The process can vary depending on the nature of the program, the resources available, and the target population.",
+  },
+  {
+    question: "Can we donate in cash or goods?",
+    answer:
+      'We accept both. If you prefer to donate cash, just click the "Make a Donation" button bove.If you prefer to donate goods, please <a class="text-brand-color underline transition-all hover:text-brand-color-darker" href="https://m.me/cebuunitedcarcampers" target="_blank">send us a message in our official Facebook page</a> for a list of what can be given to our chosen beneficiary as per the organization\'s rules and guidelines',
+  },
+  {
+    question: "Do I need to be a part of CUCC to make a donation?",
+    answer: "No! Anyone and everyone is welcome to make a donation.",
+  },
+  {
+    question:
+      "Do I need to be a part of CUCC to participate in the outreach program?",
+    answer:
+      "No! Anyone and everyone can take part in our activities. We encourage as many volunteers as possible given that it is within the limits set by our beneficiary.",
+  },
+  {
+    question: "How do I volunteer to take part in the outreach activity?",
+    answer:
+      'We will open a registration form soon. For the meantime, we will need you to <a class="text-brand-color underline transition-all hover:text-brand-color-darker" href="https://m.me/cebuunitedcarcampers" target="_blank">contact us via our official Facebook page</a> and state your interest in volunteering.',
+  },
+];
+const FAQ = () => {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      {faqItems.map((item, index) => {
+        return (
+          <AccordionItem value={index.toString()} key={index}>
+            <AccordionTrigger className="h5-heading-sizing text-left font-serif text-slate-600 hover:text-slate-700 hover:no-underline">
+              {item.question}
+            </AccordionTrigger>
+            <AccordionContent className="text-slate-500 ~text-sm/base">
+              {parser(item.answer)}
+            </AccordionContent>
+          </AccordionItem>
+        );
+      })}
+    </Accordion>
+  );
+};
+
+export { FAQ };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -65,8 +65,30 @@ export default {
 				sm: 'calc(var(--radius) - 4px)'
 			},
 			gridTemplateColumns: {
-        timeline: 'auto auto minmax(0,1fr)',
-      },
+				timeline: 'auto auto minmax(0,1fr)'
+			},
+			keyframes: {
+				'accordion-down': {
+					from: {
+						height: '0'
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)'
+					}
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)'
+					},
+					to: {
+						height: '0'
+					}
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out'
+			}
 		}
     },
 	plugins: [
